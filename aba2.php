3ff06f4d4cc2a5dacfd87ecf084d8f96d9467215
JoaoPauloBandeira <joaopaulo.band@hotmail.com>
2015-08-14 15:22:37 -0300

    Aba3 added

---
+++ aba2.php
@@ -0,0 +1,105 @@
+<meta charset="utf-8">
+<html>
+<head>
+    <link  rel="stylesheet" href="css/style.css">				
+    <script>
+    </script>
+    <style>
+		.nota{
+			width:40px;
+			height: 27px;
+			font-family: Trebuchet MS, sans-serif;
+			font-size:14pt;
+			border:none;
+			background-color:#2c2c2d;
+		}
+		.obs2{
+			width:250px;
+			height: 27px;
+			font-family: Trebuchet MS, sans-serif;
+			font-size: 14pt;
+			border: none;
+			background-color:#2c2c2d;
+			
+		}
+		input:focus{
+			outline: none;
+		}
+		#lightbox1 {
+	    position: absolute;
+	    top: 0%;
+	    left:0%;
+	    width: 660px;
+	    height: 300px;
+	    background-color: white;
+	    z-index: 101;
+	    padding: 14px;
+	    border-radius: 3px;
+	    display: none;
+	    box-shadow: 1px 1px 1px;
+		}
+    </style>
+</head>
+<body>
+        <div class="boxaba2">
+            <div class="info">
+            	<a style="float:left; color: white;margin-left:10px;top:10px">Como eu me avaliaria</a>
+            </div>            
+					<table border="0" style="font-family: Trebuchet MS, sans-serif;font-size:17px;width:590px;height:300px;margin-left:auto;margin-right:auto;position:relative;top:10px;">
+
+						<tr style="background-color:#2c2c2d;color:white;height:30px;position:relative;right:110px">
+							<th class="quesito"> Quesito</th>
+							<th class="nota"> NT</th>
+							<th class="obs2"> Obs.</th>
+						</tr>
+							<tr style="background-color:#dedede;">
+								<td>União e coperação</td>
+								<td></td>
+								<td></td>
+							</tr>
+							<tr style="background-color:#dedede;">
+								<td>Disciplina</td>
+								<td></td>
+								<td></td>
+							</tr>
+							<tr style="background-color:#dedede;">
+								<td>Dedicação</td>
+								<td></td>
+								<td></td>
+							</tr>
+							<tr style="background-color:#dedede;">
+								<td>Consciência Crítica</td>
+								<td></td>
+								<td></td>
+							</tr>
+							<tr style="background-color:#dedede;">
+								<td>Bom domínio e agilidade nas rotinas</td>
+								<td></td>
+								<td></td>
+							</tr>
+							<tr style="background-color:#dedede;">
+								<td>Boa organização nos processos</td>
+								<td></td>
+								<td></td>
+							</tr>
+							<tr style="background-color:#dedede;">
+								<td>Boa criatividade</td>
+								<td></td>
+								<td></td>
+							</tr>
+							<tr style="background-color:#dedede;">
+								<td>Boa interação com seus coordenadores</td>
+								<td></td>
+								<td></td>
+							</tr>
+							<tr style="background-color:#dedede;">
+								<td>Iniciativa</td>
+								<td></td>
+								<td></td>
+							</tr>			
+					</table>
+        
+        </div>
+
+</body>
+</html>
